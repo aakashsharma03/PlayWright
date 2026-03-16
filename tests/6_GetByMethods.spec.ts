335c650413db612f42dddb975c3849374507b0a4
aakashsharma03 <aakashsharma.3@gmail.com>
2026-03-16 22:10:39 +0530

    GetBy

--- tests/6_GetByMethods.spec.ts
+++ tests/6_GetByMethods.spec.ts
@@ -31,4 +31,28 @@ test ("Get by methods in Playwright", async ({page}) => {
     // It is used to find the element by its alt text.
     const logo = page.getByAltText("nopCommerce demo store");
     console.log(await logo.getAttribute("src"));
+
+
+    
+
+   // await page.goto("https://demo.nopcommerce.com/electronics");
+    //getByTitle
+    //syntax page.getByTitle('title text')
+    // It is used to find the element by its title attribute.
+    //const title1 = page.getByTitle("Show products in category Others").first();
+    //console.log(await title1.textContent());
+
+    
+    //getByRole
+    //syntax page.getByRole('role', {name: 'accessible name'})
+    // It is used to find the element by its role and accessible name.
+    //await page.getByRole('button', { name: 'Search' }).click();
+
+        //getByTestId
+    //syntax page.getByTestId('test id')
+    // It is used to find the element by its test id.
+    
+    await page.goto("https://www.saucedemo.com/");
+    const testId = page.getByTestId("username");
+    await testId.fill("skycodeqa@gmail.com");
 })
